d2761bc13033475c3756795a78d946454ce2439b
Taihua Li <taihua.ray.li@gmail.com>
2016-05-11 14:35:07 -0500

    Update Subset.R

--- Preprocessing/Subset.R
+++ Preprocessing/Subset.R
@@ -4,5 +4,5 @@ variables <- c("seqno", "rectype", "origyear", "licnstat", "licnfeld",
                "outcome", "malyear1", "malyear2", "payment", "totalpmt",
                "paynumbr", "numbprsn", "paytype", "ptage", "ptgender",
                "pttype", "aaclass1", "practnum")
-new_dat <- subset(dat, rectype == 'M' | rectype == 'P', select=variables)
-new_dat <- subset(new_dat, licnfeld == 10 | licnfeld == 15 | licnfeld == 20 | licnfeld == 25)
+new_dat <- subset(dat, rectype == 'M' | rectype == 'P', select=variables) # M&P only (malpractice payments)
+new_dat <- subset(new_dat, licnfeld == 10 | licnfeld == 15 | licnfeld == 20 | licnfeld == 25) # doctors only
